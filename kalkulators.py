1bc74de361ef674e882c321f38efd68b698dd203
leldeberzina <145644623+leldeberzina@users.noreply.github.com>
2024-05-16 08:51:34 +0300

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -2,6 +2,22 @@ from tkinter import*
 mansLogs=Tk()
 mansLogs.title("Kalkulators")
 
+def btnClick(number):
+    current=e.get()#nolasa skaitli
+    e.delete(0,END)#nodzēš
+    newNumber=str(current)+str(number)
+    e.insert(0,newNumber)#ievieto displejā
+    return 0
+
+def btnCommand(command):
+    global number
+    global num1 #jāiegaumē skaitlis, darbība
+    global mathOp#matemātiskais operators
+    mathOp=command
+    num1=int(e.get())
+    e.delete(0,END)
+    return 0
+
 btn0=Button(mansLogs,text='0',padx='40',pady='20')
 btn1=Button(mansLogs,text='1',padx='40',pady='20')
 btn2=Button(mansLogs,text='2',padx='40',pady='20')
@@ -12,5 +28,37 @@ btn6=Button(mansLogs,text='6',padx='40',pady='20')
 btn7=Button(mansLogs,text='7',padx='40',pady='20')
 btn8=Button(mansLogs,text='8',padx='40',pady='20')
 btn9=Button(mansLogs,text='9',padx='40',pady='20')
+
+btn7.grid(row=1,column=0)
+btn8.grid(row=1,column=1)
+btn9.grid(row=1,column=2)
+
+btn4.grid(row=2,column=0)
+btn5.grid(row=2,column=1)
+btn6.grid(row=2,column=2)
+
+btn1.grid(row=3,column=0)
+btn2.grid(row=3,column=1)
+btn3.grid(row=3,column=2)
+
+btn0.grid(row=4,column=0)
+
+e=Entry(mansLogs,width=17,bd=7,font=("Arial Black",20),justify="right") #displejs
+e.grid(row=0,columnspan=4)
+
+btnpluss=Button(mansLogs,text='+',padx='40',pady='20')
+btnmin=Button(mansLogs,text='-',padx='40',pady='20')
+btndal=Button(mansLogs,text='/',padx='40',pady='20')
+btnreiz=Button(mansLogs,text='*',padx='40',pady='20')
+btnvien=Button(mansLogs,text='=',padx='40',pady='20')
+btnc=Button(mansLogs,text='C',padx='40',pady='20')
+
+btnpluss.grid(row=1,column=3)
+btnmin.grid(row=2,column=3)
+btnreiz.grid(row=3,column=3)
+btndal.grid(row=4,column=3)
+btnvien.grid(row=4,column=2)
+btnreiz.grid(row=3,column=3)
+btnc.grid(row=4,column=1)
 
 mansLogs.mainloop()
