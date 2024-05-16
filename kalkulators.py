15f109a7dfd46d235e1c1530419edc6b5d5dc2c2
leldeberzina <145644623+leldeberzina@users.noreply.github.com>
2024-05-16 08:31:25 +0300

    Create kalkulators.py

---
+++ kalkulators.py
@@ -0,0 +1,16 @@
+from tkinter import*
+mansLogs=Tk()
+mansLogs.title("Kalkulators")
+
+btn0=Button(mansLogs,text='0',padx='40',pady='20')
+btn1=Button(mansLogs,text='1',padx='40',pady='20')
+btn2=Button(mansLogs,text='2',padx='40',pady='20')
+btn3=Button(mansLogs,text='3',padx='40',pady='20')
+btn4=Button(mansLogs,text='4',padx='40',pady='20')
+btn5=Button(mansLogs,text='5',padx='40',pady='20')
+btn6=Button(mansLogs,text='6',padx='40',pady='20')
+btn7=Button(mansLogs,text='7',padx='40',pady='20')
+btn8=Button(mansLogs,text='8',padx='40',pady='20')
+btn9=Button(mansLogs,text='9',padx='40',pady='20')
+
+mansLogs.mainloop()
